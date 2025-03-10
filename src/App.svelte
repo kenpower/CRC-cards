4795697a6f88b0f0fb501f940c308ea85bce3dd4
kenpower <github@kenpower.com>
2025-03-10 00:41:36 +0000

    use auth service login

--- src/App.svelte
+++ src/App.svelte
@@ -52,31 +52,65 @@
   let userName = $state();
   let profileIcon = $state();
 
-  let user = null;
-  onMount(() => {
-    const googleToken = localStorage.getItem("google-token");
+  let user = $state(null);
+  
+  const user_from_login_token = (event) => {
+    const params = new URLSearchParams(window.location.search);
+    const token = params.get('token');
 
-    //fetchDocument();
-    //listenForChanges();
+    if (token) {
+      console.log('token found, decoding'); //decode the jwt token
+      const AUTH_SERVICE_JWT_SECRET_KEY = import.meta.env.AUTH_SERVICE_JWT_SECRET_KEY;
 
-    if (googleToken) {
-      google_signed_in = true;
-      user = JSON.parse(googleToken);
-      profileIcon = user.picture;
-      userName = user.name;
+      try {
+        const user = jwt.verify(token, AUTH_SERVICE_JWT_SECRET_KEY);
+
+        user.auth_service_id = user.user_id; //rename user_id to auth_service_id to match the database schema and avoid confusion over which id is which
+        delete user.user_id;
+        return user;
+      } catch (error) {
+        console.log('Error decoding login token:', error);
+        console.log('token:', token);
+      }
     }
+    console.log('No token found in URL');
+    console.log('url', window.location);
+
+    return null;
+};
+  onMount(() => {
+
+    const user = localStorage.getItem("user");
+    
+    if (!user) {
+      console.log('No user found in local storage');
+      const user = user_from_login_token();
+      if (user) {
+        localStorage.setItem("user", JSON.stringify(user));
+        window.location.href = window.location.origin;
+      }
+      else{
+        console.log('No auth token found, redirecting to login');
+        const home_url = import.meta.env.PUBLIC_URL || window.location.origin;
+        const redirect_url = home_url;
+        window.location.href = `https://compucore.itcarlow.ie/auth/sign_in?redirect=${redirect_url}`;
+      }
+    }
+
+
   });
 
   function setProjectId(value) {
     console.log("Setting project ID to", value);
     projectId = value;
   }
+
 </script>
 
 <svelte:window bind:innerWidth bind:innerHeight />
 
-{#if !google_signed_in}
-  <Signin />
+{#if !user}
+  Redirecting to authentication...
 {:else}
   <div class="flexy full-screen">
     <TopBar {userName} {profileIcon} {setProjectId} />
